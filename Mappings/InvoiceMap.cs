092912ef6f090b4e39bffcf840e5b6f01826d124
Oleg Savelos <olegsavelos@gmail.com>
2022-03-29 20:58:36 +0300

    Invoice map cleanup.

--- Mappings/InvoiceMap.cs
+++ Mappings/InvoiceMap.cs
@@ -1,10 +1,5 @@
 ﻿using GizmoDALV2.Entities;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GizmoDALV2.Mappings
 {
@@ -12,49 +7,49 @@ namespace GizmoDALV2.Mappings
     {
         public InvoiceMap()
         {
-            this.HasKey(x => x.Id);
+            HasKey(x => x.Id);
 
-            this.ToTable(nameof(Invoice));
-
-            this.Property(x => x.Id)
+            Property(x => x.Id)
                 .HasColumnOrder(0)
                 .HasColumnName("InvoiceId");
 
-            this.Property(x => x.ProductOrderId)
+            Property(x => x.ProductOrderId)
                 .HasColumnOrder(1);
 
-            this.Property(x => x.UserId)
+            Property(x => x.UserId)
                 .HasColumnOrder(2);
 
-            this.Property(x => x.Status)
+            Property(x => x.Status)
                 .HasColumnOrder(3);
 
-            this.Property(x => x.SubTotal)
+            Property(x => x.SubTotal)
                 .HasColumnOrder(4);
 
-            this.Property(x => x.PointsTotal)
+            Property(x => x.PointsTotal)
                 .HasColumnOrder(5);
 
-            this.Property(x => x.TaxTotal)
+            Property(x => x.TaxTotal)
                 .HasColumnOrder(6);
 
-            this.Property(x => x.Total)
+            Property(x => x.Total)
                 .HasColumnOrder(7);
 
-            this.Property(x => x.Outstanding)
+            Property(x => x.Outstanding)
                 .HasColumnOrder(8);
 
-            this.Property(x => x.OutstandngPoints)
+            Property(x => x.OutstandngPoints)
                 .HasColumnOrder(9);
 
-            this.HasRequired(x => x.ProductOrder)
+            HasRequired(x => x.ProductOrder)
                 .WithMany(x => x.Invoices)
                 .HasForeignKey(x => x.ProductOrderId);
 
-            this.HasRequired(x => x.User)
+            HasRequired(x => x.User)
                 .WithMany(x => x.Invoices)
                 .HasForeignKey(x => x.UserId)
-                .WillCascadeOnDelete(false);       
+                .WillCascadeOnDelete(false);
+
+            ToTable(nameof(Invoice));
         }
     }
 }
