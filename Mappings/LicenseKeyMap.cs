8330532617708fa6917630af455eb2a7fd584337
Oleg Savelos <olegsavelos@gmail.com>
2022-06-06 10:08:17 +0300

    Updates.

--- Mappings/LicenseKeyMap.cs
+++ Mappings/LicenseKeyMap.cs
@@ -1,38 +1,42 @@
 ﻿using GizmoDALV2.Entities;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.Infrastructure.Annotations;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GizmoDALV2.Mappings
 {
+    /// <summary>
+    /// License key map.
+    /// </summary>
     public class LicenseKeyMap : EntityTypeConfiguration<LicenseKey>
     {
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
         public LicenseKeyMap()
         {
             // Primary Key
-            this.HasKey(t => t.Id);
+            HasKey(t => t.Id);
+
+            Property(t => t.Id)
+                .HasColumnName("LicenseKeyId");
 
             // Properties
-            this.Property(x => x.Id)
+            Property(x => x.Id)
                 .HasColumnOrder(0);
 
-            this.Property(t => t.LicenseId)
+            Property(t => t.LicenseId)
                 .HasColumnOrder(1);
 
-            this.Property(x => x.Value)
+            Property(x => x.Value)
                 .HasColumnOrder(2)
                 .HasMaxLength(SQLByteArraySize.NORMAL);
 
-            this.Property(t => t.Comment)
+            Property(t => t.Comment)
                 .HasMaxLength(SQLStringSize.TINY)
                 .HasColumnOrder(3);            
 
-            this.Property(x => x.Guid)
+            Property(x => x.Guid)
                 .HasColumnOrder(4)
                 .HasColumnAnnotation("Index",
                 new IndexAnnotation(new[] 
@@ -40,19 +44,24 @@ namespace GizmoDALV2.Mappings
                     new IndexAttribute("UQ_Guid") { IsUnique = true } 
                 }));
 
-            this.Property(x => x.IsEnabled)
+            Property(x => x.IsEnabled)
                 .HasColumnOrder(5);
 
-            // Table & Column Mappings
-            this.ToTable("LicenseKey");
+            Property(x => x.AssignedHostId)
+                .HasColumnOrder(6)
+                .IsOptional();
 
-            this.Property(t => t.Id)
-                .HasColumnName("LicenseKeyId");
+            // Table & Column Mappings
+            ToTable(nameof(LicenseKey));      
 
             // Relationships
-            this.HasRequired(t => t.License)
+            HasRequired(t => t.License)
                 .WithMany(t => t.LicenseKeys)
                 .HasForeignKey(d => d.LicenseId);
+
+            HasOptional(x => x.AssignedHost)
+                .WithMany()
+                .HasForeignKey(x => x.AssignedHostId);
         }
     }
 }
