e271953d91f39ee7c9c83899d5bd554c9344d159
Andrey Pestunov <masterlifting@gmail.com>
2023-11-08 08:09:28 +0100

    Add AssistanceRequest mappings

---
+++ Gizmo.DAL.EFCore/Mappings/AssistanceRequestMap.cs
@@ -0,0 +1,54 @@
+﻿using Gizmo.DAL.Entities;
+
+using GizmoDALV2;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gizmo.DAL.Mappings
+{
+    public class AssistanceRequestMap : IEntityTypeConfiguration<AssistanceRequest>
+    {
+        /// <summary>
+        /// Configure entity
+        /// </summary>
+        public void Configure(EntityTypeBuilder<AssistanceRequest> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnOrder(0)
+                .HasColumnName("AssistanceRequestId");
+
+            builder.Property(x => x.UserId)
+                .IsRequired(false)
+                .HasColumnOrder(1);
+
+            builder.Property(x => x.HostId)
+                .IsRequired()
+                .HasColumnOrder(2);
+
+            builder.Property(x => x.AssistanceRequestTypeId)
+                .IsRequired()
+                .HasColumnOrder(3);
+
+            builder.Property(x => x.Note)
+                .HasColumnOrder(4)
+                .HasMaxLength(SQLStringSize.TINY)
+                .IsRequired(false);
+
+            builder.Property(x => x.Status)
+                .HasColumnOrder(5);
+
+            builder.HasOne(x => x.AssistanceRequestType)
+                .WithMany(x => x.AssistanceRequests)
+                .HasForeignKey(x => x.AssistanceRequestTypeId);
+
+            builder.HasOne(x => x.Host)
+                .WithMany(x => x.AssitanceRequests)
+                .HasForeignKey(x => x.HostId);
+
+            builder.ToTable(nameof(AssistanceRequest));
+        }
+    }
+}
---
+++ Gizmo.DAL.EFCore/Mappings/AssistanceRequestTypeMap.cs
@@ -0,0 +1,37 @@
+﻿using Gizmo.DAL.Entities;
+
+using GizmoDALV2;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gizmo.DAL.Mappings
+{
+    public class AssistanceRequestTypeMap : IEntityTypeConfiguration<AssistanceRequestType>
+    {
+        /// <summary>
+        /// Configure entity
+        /// </summary>
+        public void Configure(EntityTypeBuilder<AssistanceRequestType> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnOrder(0)
+                .HasColumnName("AssistanceRequestTypeId");
+
+            builder.Property(x => x.Title)
+                .HasColumnOrder(1)
+                .HasMaxLength(SQLStringSize.TINY45)
+                .IsRequired();
+
+            builder.Property(x => x.DisplayOrder)
+                .HasColumnOrder(2);
+
+            builder.Property(x => x.IsDeleted)
+                .HasColumnOrder(3);
+
+            builder.ToTable(nameof(AssistanceRequestType));
+        }
+    }
+}
