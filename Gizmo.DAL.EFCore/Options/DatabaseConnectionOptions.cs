eeb20f4a21148d54331d618f24da854facc66fe6
Oleg Savelos <olegsavelos@gmail.com>
2023-11-12 10:59:40 +0200

    db connection settings class.

---
+++ Gizmo.DAL.EFCore/Options/DatabaseConnectionOptions.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+
+namespace Gizmo.DAL.EFCore
+{
+    /// <summary>
+    /// Database connection options.
+    /// </summary>
+    /// <remarks>
+    /// This will be directly mapped to an application.json or any other source configuration file.<br></br>
+    /// <b>This structure mirrors current Gizmo server connection options class.</b>
+    /// </remarks>
+    public class DatabaseConnectionOptions
+    {
+        /// <summary>
+        /// Type of the database.
+        /// </summary>
+        public SharedLib.DatabaseType DbType
+        {
+            get;
+            init;
+        }
+
+        /// <summary>
+        /// Database connection string.
+        /// </summary>
+        public string DbConnectionString
+        {
+            get;
+            init;
+        } = string.Empty;
+
+        /// <summary>
+        /// Optional command timeout.
+        /// </summary>
+        public int? CommandTimeout
+        {
+            get; init;
+        }
+    }
+}
