cd177851140114084d311cb233289cba0c9c625b
Oleg Savelos <olegsavelos@gmail.com>
2024-07-31 14:22:59 +0300

    insert default branch.

--- Migrations/Gizmo.DAL.Migrations.MSSQL/Scripts.cs
+++ Migrations/Gizmo.DAL.Migrations.MSSQL/Scripts.cs
@@ -3,6 +3,10 @@
     public static class Scripts
     {
         public static readonly string BRANCH_SET = """
+            IF NOT EXISTS (SELECT BranchId FROM Branch)
+            BEGIN
+            INSERT INTO Branch (Name,Guid,IsEnabled,IsDeleted,CreatedTime) VALUES ('Default',NEWID(),1,0,GETDATE())
+            END
             UPDATE [dbo].[Register] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
             UPDATE [dbo].[Asset] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
             UPDATE [dbo].[Device] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
--- Migrations/Gizmo.DAL.Migrations.Npgsql/Scripts.cs
+++ Migrations/Gizmo.DAL.Migrations.Npgsql/Scripts.cs
@@ -3,6 +3,10 @@
     public static class Scripts
     {
         public static readonly string BRANCH_SET = """
+            IF NOT EXISTS (SELECT BranchId FROM Branch)
+            BEGIN
+            INSERT INTO Branch (Name,Guid,IsEnabled,IsDeleted,CreatedTime) VALUES ('Default',NEWID(),1,0,GETDATE())
+            END
             UPDATE "Register" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
             UPDATE "Asset" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
             UPDATE "Device" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
