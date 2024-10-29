76ce940fb862e9476f57b125beef4b94f9fa9d0c
klapsis-netprojects <klapsis@netprojects.gr>
2024-10-29 16:11:31 +0200

    Revert.

--- DAL/GizmoDAL.cs
+++ DAL/GizmoDAL.cs
@@ -758,9 +758,6 @@ namespace Gizmo.DAL
 
                         await cx.Database.ExecuteSqlCommandAsync("UPDATE [dbo].[AssistanceRequest] Set CreatedById=NULL", ct);
                         await cx.Database.ExecuteSqlCommandAsync("UPDATE [dbo].[AssistanceRequest] Set ModifiedById=NULL", ct);
-
-                        await cx.Database.ExecuteSqlCommandAsync("UPDATE [dbo].[ReportPreset] Set CreatedById=NULL", ct);
-                        await cx.Database.ExecuteSqlCommandAsync("UPDATE [dbo].[ReportPreset] Set ModifiedById=NULL", ct);
                     }
 
                     if (!deleteHosts)
