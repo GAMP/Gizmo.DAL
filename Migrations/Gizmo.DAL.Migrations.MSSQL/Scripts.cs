eb7e7b3d82c48e66217f73d80b2ac6a4ad720430
Oleg Savelos <olegsavelos@gmail.com>
2024-11-06 10:28:40 +0200

    update scripts.

--- Migrations/Gizmo.DAL.Migrations.MSSQL/Scripts.cs
+++ Migrations/Gizmo.DAL.Migrations.MSSQL/Scripts.cs
@@ -2,10 +2,13 @@
 {
     public static class Scripts
     {
-        public static readonly string BRANCH_SET = """
+        /// <summary>
+        /// This script will create a branch and set it to dependent entities while migrating EF6 database.
+        /// </summary>
+        public static readonly string EF_6_BRANCH_SET = """
             IF NOT EXISTS (SELECT BranchId FROM Branch)
             BEGIN
-            INSERT INTO Branch (Name,Guid,IsEnabled,IsDeleted,CreatedTime) VALUES ('Default',NEWID(),1,0,GETDATE())
+            INSERT INTO Branch (Name,Guid,IsEnabled,IsDeleted,CreatedTime,Latitude,Longitude) VALUES ('Main',NEWID(),1,0,GETDATE(),0,0)
             END
             UPDATE [dbo].[Register] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
             UPDATE [dbo].[Asset] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
@@ -15,6 +18,11 @@
             UPDATE [dbo].[Shift] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
             UPDATE [dbo].[StockTransaction] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
             UPDATE [dbo].[Reservation] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
+            UPDATE [dbo].[AppStat] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
+            UPDATE [dbo].[UsageSession] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
+            UPDATE [dbo].[UserSession] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
+            UPDATE [dbo].[AssetTransaction] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
+            UPDATE [dbo].[AssistanceRequest] Set BranchId=(SELECT min(BranchId) FROM [dbo].[Branch]);
         """;
     }
 }
