48ff47973ddb0b474dbf88bd5f62ef9bceaf41f0
Oleg Savelos <olegsavelos@gmail.com>
2024-11-25 18:32:30 +0200

    permission set.

---
+++ Mappings/UserPermissionSetMap.cs
@@ -0,0 +1,29 @@
+﻿using Gizmo.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gizmo.DAL.Mappings
+{
+    /// <summary>
+    /// User permission set map.
+    /// </summary>
+    public sealed class UserPermissionSetMap : IEntityTypeConfiguration<UserPermissionSet>
+    {
+        ///<inheritdoc/>
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<UserPermissionSet> builder)
+        {
+            builder.ToTable(nameof(UserPermissionSet));
+
+            builder.HasKey(permissionSet => permissionSet.Id);
+            builder.Property(permissionSet => permissionSet.Id)
+                .HasColumnName("UserPermissionSetId")
+                .HasColumnOrder(0);
+
+            builder.Property(permissionSet => permissionSet.Name)
+                .HasColumnOrder(1)
+                .HasMaxLength(SQLStringSize.TINY45)
+                .IsRequired();
+
+            builder.HasIndex(permissionSet => permissionSet.Name).IsUnique().HasFilter(null);
+        }
+    }
+}
---
+++ Mappings/UserPermissionSetPermissionMap.cs
@@ -0,0 +1,47 @@
+﻿using Gizmo.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gizmo.DAL.Mappings
+{
+    /// <summary>
+    /// User permission set permission map.
+    /// </summary>
+    public sealed class UserPermissionSetPermissionMap : IEntityTypeConfiguration<UserPermissionSetPermission>
+    {
+        ///<inheritdoc/>
+        public void Configure(EntityTypeBuilder<UserPermissionSetPermission> builder)
+        {
+            builder.ToTable(nameof(UserPermissionSetPermission));
+
+            builder.HasKey(userPermissionSetPermission => userPermissionSetPermission.Id);
+            builder.Property(userPermissionSetPermission => userPermissionSetPermission.Id)
+                .HasColumnName("UserPermissionSetPermissionId")
+                .HasColumnOrder(0);
+
+            builder.Property(userPermissionSetPermission => userPermissionSetPermission.PermissionSetId)
+                .HasColumnOrder(1)
+                .IsRequired();
+
+            builder.Property(userPermissionSetPermission => userPermissionSetPermission.Type)
+                .HasColumnOrder(2)
+                .HasMaxLength(SQLStringSize.TINY45)
+                .IsRequired();
+
+            builder.Property(userPermissionSetPermission => userPermissionSetPermission.Value)
+                .HasColumnOrder(3)
+                .HasMaxLength(SQLStringSize.TINY45)
+                .IsRequired();
+
+            builder.HasIndex(UserPermissionSetPermission => new { UserPermissionSetPermission.PermissionSetId, UserPermissionSetPermission.Type, UserPermissionSetPermission.Value })
+                .IsUnique()
+                .HasFilter(null);
+
+            builder.HasOne(userPermissionSetPermission => userPermissionSetPermission.PermissionSet)
+                .WithMany(userPermissionSet => userPermissionSet.Permissions)
+                .HasForeignKey(userPermissionSetPermission => userPermissionSetPermission.PermissionSetId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
