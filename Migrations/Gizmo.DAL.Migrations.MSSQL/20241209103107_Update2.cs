ae2f03a632531e6abde4449f0cab1b9a348a88f2
Oleg Savelos <olegsavelos@gmail.com>
2024-12-09 17:51:23 +0200

    updates.

---
+++ Migrations/Gizmo.DAL.Migrations.MSSQL/20241209103107_Update2.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gizmo.DAL.Migrations.MSSQL
+{
+    /// <inheritdoc />
+    public partial class Update2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Register_Name_BranchId",
+                table: "Register");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Register_Name_BranchId",
+                table: "Register",
+                columns: new[] { "Name", "BranchId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Register_Name_BranchId",
+                table: "Register");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Register_Name_BranchId",
+                table: "Register",
+                columns: new[] { "Name", "BranchId" });
+        }
+    }
+}
