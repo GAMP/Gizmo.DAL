8c75b82cafae22a2c57dc366f90506b169593f5b
Oleg Savelos <olegsavelos@gmail.com>
2024-12-19 13:28:38 +0200

    add product age restrictions.

---
+++ Mappings/AgeRestrictionProductMap.cs
@@ -0,0 +1,32 @@
+﻿using Gizmo.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gizmo.DAL.Mappings
+{
+    /// <summary>
+    /// Age restriction product entity map.
+    /// </summary>
+    public sealed class AgeRestrictionProductMap : IEntityTypeConfiguration<AgeRestrictionProduct>
+    {
+        /// <inheritdoc/>
+        public void Configure(EntityTypeBuilder<AgeRestrictionProduct> builder)
+        {
+            builder.ToTable(nameof(AgeRestrictionProduct))
+                .HasBaseType<AgeRestriction>();
+
+            builder.Property(ageRestrictionProduct => ageRestrictionProduct.ProductId)
+                .IsRequired()
+                .HasColumnOrder(0);
+
+            builder.HasIndex(ageRestrictionProduct => ageRestrictionProduct.ProductId)
+                .IsUnique()
+                .HasFilter(null);
+
+            builder.HasOne(ageRestrictionProduct => ageRestrictionProduct.Product)
+                .WithMany(product => product.AgeRestrictions)
+                .HasForeignKey(ageRestrictionProduct => ageRestrictionProduct.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
