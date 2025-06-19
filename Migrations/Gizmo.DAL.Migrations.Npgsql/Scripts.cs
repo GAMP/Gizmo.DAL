c03da362e769cbbdfb7bb016161f8710e86a7553
Oleg Savelos <olegsavelos@gmail.com>
2025-06-19 10:32:20 +0300

    use deposit payment amount.

--- Migrations/Gizmo.DAL.Migrations.Npgsql/Scripts.cs
+++ Migrations/Gizmo.DAL.Migrations.Npgsql/Scripts.cs
@@ -2,19 +2,5 @@
 {
     public static class Scripts
     {
-        public static readonly string BRANCH_SET = """
-            IF NOT EXISTS (SELECT BranchId FROM Branch)
-            BEGIN
-            INSERT INTO Branch (Name,Guid,IsEnabled,IsDeleted,CreatedTime) VALUES ('Default',NEWID(),1,0,GETDATE())
-            END
-            UPDATE "Register" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
-            UPDATE "Asset" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
-            UPDATE "Device" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
-            UPDATE "HostGroup" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
-            UPDATE "HostLayoutGroup" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
-            UPDATE "Shift" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
-            UPDATE "StockTransaction" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
-            UPDATE "Reservation" SET "BranchId" = (SELECT min("BranchId") FROM "Branch");
-        """;
     }
 }
