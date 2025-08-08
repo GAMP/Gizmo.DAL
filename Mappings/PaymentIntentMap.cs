4a7e9efd8e1d2b05e736325ca0ece2bb36078578
Oleg Savelos <olegsavelos@gmail.com>
2025-08-08 11:39:03 +0300

    updates.

--- Mappings/PaymentIntentMap.cs
+++ Mappings/PaymentIntentMap.cs
@@ -54,10 +54,18 @@ namespace Gizmo.DAL.Mappings
                 .HasColumnOrder(8)
                 .IsRequired();
 
-            builder.Property(paymentIntent => paymentIntent.PaymentLinkUrl)
+            builder.Property(paymentIntent => paymentIntent.PaymentUrl)
                 .HasColumnOrder(9)
                 .IsRequired(false)
                 .HasMaxLength(SQLStringSize.TINY);
+
+            builder.Property(paymentIntent => paymentIntent.Expiration)
+                .HasColumnOrder(10)
+                .IsRequired(false);
+
+            builder.Property(paymentIntent => paymentIntent.ExpireAt)
+                .HasColumnOrder(11)
+                .IsRequired(false);
 
             builder.HasOne(paymentIntent => paymentIntent.User)
                 .WithMany(userMember => userMember.PaymentIntents)
