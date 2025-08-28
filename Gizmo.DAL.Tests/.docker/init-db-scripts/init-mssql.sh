ccf315aff79f53eb325b70bd9aa2a099ef54d376
Andrei Pestunov <masterlifting@gmail.com>
2025-08-28 14:35:39 +0200

    Simplify database initialization scripts for SQL Server and PostgreSQL by consolidating DROP and CREATE commands into single statements.

--- Gizmo.DAL.Tests/.docker/init-db-scripts/init-mssql.sh
+++ Gizmo.DAL.Tests/.docker/init-db-scripts/init-mssql.sh
@@ -6,24 +6,13 @@
 # Wait for SQL Server to be ready
 sleep 10
 
-# Check if Gizmo database exists
-DB_EXISTS=$(/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "${DB_PASSWORD}" -C -Q "SELECT COUNT(*) FROM sys.databases WHERE name = '${DB_NAME}'" -h -1 2>/dev/null | head -1 | tr -d ' ')
-
-if [ "$DB_EXISTS" -eq 1 ]; then
-    /opt/mssql-tools18/bin/sqlcmd \
-        -S localhost \
-        -U sa \
-        -P "${DB_PASSWORD}" \
-        -C \
-        -Q "USE [master]; DROP DATABASE ${DB_NAME};"
-fi
-
 /opt/mssql-tools18/bin/sqlcmd \
     -S localhost \
     -U sa \
     -P "${DB_PASSWORD}" \
     -C \
     -Q "  USE [master];
+            DROP DATABASE IF EXISTS ${DB_NAME};
             RESTORE DATABASE ${DB_NAME} \
             FROM DISK = '/var/opt/mssql/backups/${BACKUP_FILE}' \
             WITH MOVE '${DB_NAME}' TO '/var/opt/mssql/data/${DB_NAME}.mdf', \
