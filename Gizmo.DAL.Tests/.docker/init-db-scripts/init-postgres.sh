ba501d9d938be9e76d38aaecca6ffc9f1572e191
Andrei Pestunov <masterlifting@gmail.com>
2025-09-11 18:30:15 +0200

    Refactor database initialization commands in init-postgres.sh for clarity

--- Gizmo.DAL.Tests/.docker/init-db-scripts/init-postgres.sh
+++ Gizmo.DAL.Tests/.docker/init-db-scripts/init-postgres.sh
@@ -3,7 +3,8 @@
 # Wait for PostgreSQL to be ready
 sleep 10
 
-psql -U "${POSTGRES_USER}" -d postgres -c "DROP DATABASE IF EXISTS ${DB_NAME}; CREATE DATABASE ${DB_NAME};"
+psql -U "${POSTGRES_USER}" -d postgres -c "DROP DATABASE IF EXISTS \"${DB_NAME}\";"
+psql -U "${POSTGRES_USER}" -d postgres -c "CREATE DATABASE \"${DB_NAME}\";"
 pg_restore -U "${POSTGRES_USER}" -d "${DB_NAME}" --clean --if-exists --verbose "${BACKUP_PATH}/${BACKUP_FILE}"
 
 if [ $? -ne 0 ]; then
