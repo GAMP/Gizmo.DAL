146d4c2e3e523cb8f34f6a3af102fb0f9f0f76f6
Andrei Pestunov <masterlifting@gmail.com>
2025-09-11 18:23:57 +0200

    Remove unnecessary quotes from database commands in init-postgres.sh

--- Gizmo.DAL.Tests/.docker/init-db-scripts/init-postgres.sh
+++ Gizmo.DAL.Tests/.docker/init-db-scripts/init-postgres.sh
@@ -3,8 +3,8 @@
 # Wait for PostgreSQL to be ready
 sleep 10
 
-psql -U "${POSTGRES_USER}" -d postgres -c "DROP DATABASE IF EXISTS \"${DB_NAME}\"; CREATE DATABASE \"${DB_NAME}\";"
-pg_restore -U "${POSTGRES_USER}" -d "\"${DB_NAME}\"" --clean --if-exists --verbose "${BACKUP_PATH}/${BACKUP_FILE}"
+psql -U "${POSTGRES_USER}" -d postgres -c "DROP DATABASE IF EXISTS ${DB_NAME}; CREATE DATABASE ${DB_NAME};"
+pg_restore -U "${POSTGRES_USER}" -d "${DB_NAME}" --clean --if-exists --verbose "${BACKUP_PATH}/${BACKUP_FILE}"
 
 if [ $? -ne 0 ]; then
     echo "PostgreSQL database restore failed"
